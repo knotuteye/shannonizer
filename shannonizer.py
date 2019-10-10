6b5d96484bb1c3ab3a772bbce5abb3635e0425af
Kevin Otuteye <kevinotuteye@gmail.com>
2019-10-10 21:02:15 +0000

    .

--- shannonizer.py
+++ shannonizer.py
@@ -1,6 +1,15 @@
+import math
+
+
 def greeter():
-    print("\t S H A N N O N I Z E R")
+    print("\n\t -- S H A N N O N I Z E R -- \n")
     print("This program calculates the amount of information of an event or series of events in bits based on the probability")
+    choice = input("Enter 1 for single event or 2 for series of events: ")
+    
+
+def selfInformation(prob):
+    return math.log2(1/prob)
+
 
 if __name__ == "__main__":
     greeter()
