2546b5fcc4347e8c4ce5328e4ff9fb23538e8dd4
Kevin Otuteye <kevinotuteye@gmail.com>
2019-10-10 21:11:41 +0000

    .

--- shannonizer.py
+++ shannonizer.py
@@ -4,11 +4,19 @@ import math
 def greeter():
     print("\n\t -- S H A N N O N I Z E R -- \n")
     print("This program calculates the amount of information of an event or series of events in bits based on the probability")
-    choice = input("Enter 1 for single event or 2 for series of events: ")
-    
+
 
 def selfInformation(prob):
     return math.log2(1/prob)
+
+
+def entropy(prob_list):
+    info_list = []
+    for prob in prob_list:
+        info_list.append(prob*math.log2(1/prob))
+    sum = 0
+    for info in info_list:
+        sum += info
 
 
 if __name__ == "__main__":
